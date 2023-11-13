308c03cabbafa04f703f084a82d759ead4065ef7
luis guilherme <141044778+luisalfa999@users.noreply.github.com>
2023-11-13 12:04:16 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -2,59 +2,121 @@
 let questions = [
   {
     numb: 1,
-    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
-    answer: "Marie Curiee",
+    question: "Em que ano a seleção brasileira ganhou a primeira copa do mundo?",
+    answer: "1958",
     options: [
-      "Albert Einstein",
-      "Marie Curiee",
-      "Stephen Hawking",
-      "Linus Pauling"
+      "1958",
+      "1970",
+      "2002",
+      "1956"
     ]
   },
   {
     numb: 2,
-    question: "Qual o país mais novo do mundo?",
-    answer: "Sudão do Sul",
+    question: "Quem fez o gol da vitória do corinthians no mundial de clube de 2012",
+    answer: "Paolo Guerrero",
     options: [
-      "Timor Lestet",
-      "Montenegro",
-      "Sudão do Sul",
-      "Kosovo"
+      "Paolo Guerrero",
+      "Roger Guedes",
+      "Yuri Alberto",
+      "Cássio"
     ]
   },
   {
     numb: 3,
-    question: "Qual o nome do evento astronômico responsável pelo dia mais longo do ano?",
-    answer: "Solstício de verão",
+    question: "Quantos gols  atualmente Cristiano Ronaldo tem em sua carreira?",
+    answer: "859 gols",
     options: [
-      "Equinócio de outono",
-      "Solstício de inverno",
-      "Solstício de verão",
-      "Solstício de outono"
+      "859 gols",
+      "758 gols",
+      "630 gols",
+      "540 gols"
     ]
   },
   {
     numb: 4,
-    question: "Quantos pares de costelas um ser humano, normalmente, possui?",
-    answer: "12",
+    question: "Quantas champions tem o Real Madrid no total  atualmente ",
+    answer: "14 títulos",
     options: [
-      "12",
-      "11",
-      "14",
-      "13"
+      "14 títulos",
+      "11 títulos",
+      "10 títulos",
+      "23 títulos"
     ]
   },
   {
     numb: 5,
-    question: "Quais planetas do sistema solar realizam seu movimento de rotação em sentido horário?",
-    answer: "Vênus e Urano",
+    question: "Qual o unico jogador de futbol na história a ter três copas do mundo em sua carreira?",
+    answer: "Pelé",
     options: [
-      "Saturno e Júpiter",
-      "Vênus e Urano",
-      "Terra e Marte",
-      "Mercúrio e Saturno"
+      "Pelé",
+      "Garrincha",
+      "Gullit",
+      "David Beckham"
     ]
   },
+
+  {
+    numb: 6,
+    question: "Quem foi o último jogador a vencer a Bola de Ouro no ano de 2023?",
+    answer: "Messi",
+    options: [
+      "Messi",
+      "Benzema",
+      "Cristiano Ronaldo",
+      "Haaland"
+    ]
+  },
+
+    {
+    numb: 7,
+    question: "Quantas libertadores tem o corinthians?",
+    answer: "1 título",
+    options: [
+      "1 título",
+      "4 títulos",
+      "3 títulos",
+      "2 títulos"
+    ]
+    },
+
+    {
+    numb: 8,
+    question: "Qual o número de camisa mais famoso que Cristiano Ronaldo usou em sua carreira?",
+    answer: "7",
+    options: [
+      "10",
+      "17",
+      "9",
+      "7"
+    ]
+    },
+
+    {
+    numb: 9,
+    question: "Qual  foi o  último ano que o Brasil venceu uma copa do mundo?",
+    answer: "2002",
+    options: [
+      "2006",
+      "1980",
+      "2013",
+      "2007"
+    ]
+    },
+
+    {
+    numb: 10,
+    question: "Qual é o camisa 10 atualmente na seleção brasileira?",
+    answer: "Neymar",
+    options: [
+      "Rodrygo",
+      "Neymar",
+      "Vinícius Junior",
+      "Martinelli"
+    ]
+    },
+  
+      
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
